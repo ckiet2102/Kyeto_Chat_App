3a021fc8cc25442dd98c3cc0e5398f22fafc7d20
Tieu Chan Kiet <ckiet2102@gmail.com>
2026-08-24 14:42:18 +0700

    Remove GoogleOAuthProvider wrapper to ensure clean server-side Passport link redirect

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './lib/i18n'
 import App from './App.tsx'
-import { GoogleOAuthProvider } from '@react-oauth/google'
-
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "450643072285-naao94aegef89aqgueq8thvqp5oc9msh.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </StrictMode>,
 )
